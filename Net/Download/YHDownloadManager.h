05b2956e7e689c35d10c532f50acb7435a4e2163
liujun <1035841713@qq.com>
2019-02-17 00:19:12 +0800

    11

---
+++ Net/Download/YHDownloadManager.h
@@ -0,0 +1,17 @@
+//
+//  YHDownloadManager.h
+//  Meow
+//
+//  Created by apple on 2019/2/16.
+//  Copyright © 2019 yinhe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YHDownloadManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
