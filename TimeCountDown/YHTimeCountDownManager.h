53bea6aeb5f825d3ffa41c7b972b6506153e69c2
liujun <1035841713@qq.com>
2019-04-01 00:05:26 +0800

    cell倒计时重新整理

--- TimeCountDown/YHTimeCountDownManager.h
+++ TimeCountDown/YHTimeCountDownManager.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 extern NSString *const kYHTimeCountDownNotification;       // NotificationName
 extern NSString *const kYHTimeCountDownIdentifierKey;      // identifier
+extern NSString *const kYHTimeCountDownGroupIdentifierKey; // groupIdentifier
 extern NSString *const kYHTimeCountDownRemainingTimeKey;   // 剩余时间
 
 @interface YHTimeCountDownManager : NSObject
@@ -21,32 +22,14 @@ extern NSString *const kYHTimeCountDownRemainingTimeKey;   // 剩余时间
 + (instancetype)new NS_UNAVAILABLE;
 
 
-/**
- 开启倒计时功能
 
- @param identifier 唯一标志符
- @param interval 倒计时间隔时间
- @param startTimeInterval 开始时间戳
- @param endTimeInterval 结束时间戳
- */
-- (void)startTimeCountDownWithIdentifier:(NSString *)identifier
-                                interval:(NSTimeInterval)interval
-                       startTimeInterval:(NSTimeInterval)startTimeInterval
-                         endTimeInterval:(NSTimeInterval)endTimeInterval;
+- (void)startTimeCountDownWithGroupIdentifier:(NSString *)groupIdentifier
+                                   identifier:(NSString *)identifier
+                                     interval:(NSTimeInterval)interval
+                            startTimeInterval:(NSTimeInterval)startTimeInterval
+                              endTimeInterval:(NSTimeInterval)endTimeInterval;
 
-
-/**
- 根据某个identifer移除倒计时
-
- @param identifer 唯一标志符
- */
-- (void)removeTimerIntervalWithIdentifer:(NSString *)identifer;
-
-
-
-/**
- 移除所有倒计时
- */
+- (void)removeTimerIntervalWithGroupIdentifer:(NSString *)groupIdentifier;
 - (void)removeAllTimer;
 
 @end
--- TimeCountDown/YHTimeCountDownManager.m
+++ TimeCountDown/YHTimeCountDownManager.m
@@ -7,16 +7,34 @@
 //
 
 #import "YHTimeCountDownManager.h"
+#import <UIKit/UIKit.h>
 
 NSString *const kYHTimeCountDownNotification            = @"kYHTimeCountDownNotification";
 NSString *const kYHTimeCountDownIdentifierKey           = @"kYHTimeCountDownIdentifierKey";
+NSString *const kYHTimeCountDownGroupIdentifierKey      = @"kYHTimeCountDownGroupIdentifierKey";
 NSString *const kYHTimeCountDownRemainingTimeKey        = @"kYHTimeCountDownRemainingTimeKey";
 
 
-@interface YHTimeCountDownModel : NSObject
-@property (nonatomic, strong) dispatch_source_t timer;
-@property (nonatomic, copy) NSString *identifier;
+
+// 组
+@interface YHTimeCountDownGroup : NSObject
+@property (nonatomic, copy) NSString *groupIdentifier;
 @property (nonatomic, assign) NSTimeInterval interval;
+@property (nonatomic, strong) dispatch_source_t timer;
+@property (nonatomic, assign) BOOL isStart;
+@end
+
+
+@implementation YHTimeCountDownGroup
+
+@end
+
+
+
+// Model
+@interface YHTimeCountDownModel : NSObject
+@property (nonatomic, strong) YHTimeCountDownGroup *group;
+@property (nonatomic, copy) NSString *identifier;
 @property (nonatomic, assign) NSTimeInterval startTimeInterval;
 @property (nonatomic, assign) NSTimeInterval endTimeInterval;
 @property (nonatomic, assign) NSTimeInterval currentTimeInterval;
@@ -24,6 +42,7 @@ NSString *const kYHTimeCountDownRemainingTimeKey        = @"kYHTimeCountDownRema
 
 
 @implementation YHTimeCountDownModel
+
 
 @end
 
@@ -51,81 +70,128 @@ NSString *const kYHTimeCountDownRemainingTimeKey        = @"kYHTimeCountDownRema
     self = [super init];
     if (self) {
         self.timers = [NSMutableArray array];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(willEnterForeground:) name:UIApplicationWillEnterForegroundNotification object:nil];
     }
     return self;
 }
 
 
-- (void)startTimeCountDownWithIdentifier:(NSString *)identifier
-                                interval:(NSTimeInterval)interval
-                       startTimeInterval:(NSTimeInterval)startTimeInterval
-                         endTimeInterval:(NSTimeInterval)endTimeInterval{
-    // 先移除
-    [self removeTimerIntervalWithIdentifer:identifier];
+- (void)startTimeCountDownWithGroupIdentifier:(NSString *)groupIdentifier
+                                   identifier:(NSString *)identifier
+                                     interval:(NSTimeInterval)interval
+                            startTimeInterval:(NSTimeInterval)startTimeInterval
+                              endTimeInterval:(NSTimeInterval)endTimeInterval{
     
-    
-    YHTimeCountDownModel *model = [[YHTimeCountDownModel alloc] init];
-    model.identifier = identifier;
-    model.interval = interval;
-    
-    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
-    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, interval * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
-    dispatch_source_set_event_handler(timer, ^{
-        __block YHTimeCountDownModel *m = nil;
-        [self.timers enumerateObjectsUsingBlock:^(YHTimeCountDownModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-            if (obj.timer == timer) {
-                m = obj;
-                *stop = YES;
-            }
-        }];
-        if (m) {
-            m.currentTimeInterval += m.interval;
-            
-            NSTimeInterval remain = m.endTimeInterval - m.currentTimeInterval;
-            NSDictionary *info = @{kYHTimeCountDownIdentifierKey : m.identifier,
-                                   kYHTimeCountDownRemainingTimeKey : @(remain)};
-            
-            [[NSNotificationCenter defaultCenter] postNotificationName:kYHTimeCountDownNotification object:nil userInfo:info];
-            
-            if (m.currentTimeInterval >= m.endTimeInterval) {
-                [self removeTimerIntervalWithIdentifer:m.identifier];
-            }
+    // 根据identifier和groupIdentifier找到是否存在YHTimeCountDownModel.
+    __block YHTimeCountDownModel *tmpModel = nil;
+    [self.timers enumerateObjectsUsingBlock:^(YHTimeCountDownModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if ([obj.identifier isEqualToString:identifier] && obj.group && [obj.group.groupIdentifier isEqualToString:groupIdentifier]) {
+            tmpModel = obj;
+            *stop = YES;
         }
-    });
-    dispatch_resume(timer);
+    }];
+    if (tmpModel) {
+        return;
+    }
     
-    model.timer = timer;
-    
+    // 不存在YHTimeCountDownModel，新建；但是新建的YHTimeCountDownModel可能和timers里面的某个model有相同的groupIdentifier.
+    YHTimeCountDownModel * model = [[YHTimeCountDownModel alloc] init];
+    model.identifier = identifier;
     
     model.startTimeInterval = startTimeInterval;
     model.endTimeInterval = endTimeInterval;
-    model.currentTimeInterval = startTimeInterval;
+    model.currentTimeInterval = 0;
     
     if (startTimeInterval > endTimeInterval) {
         model.startTimeInterval = endTimeInterval;
         model.endTimeInterval = endTimeInterval;
-        model.currentTimeInterval = endTimeInterval;
+    }
+    
+    
+    
+    // 根据groupIdentifer找到group
+    __block YHTimeCountDownGroup *tmpGroup = nil;
+    [self.timers enumerateObjectsUsingBlock:^(YHTimeCountDownModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj.group && [obj.group.groupIdentifier isEqualToString:groupIdentifier]) {
+            tmpGroup = obj.group;
+            *stop = YES;
+        }
+    }];
+    
+    if (!tmpGroup) {
+        YHTimeCountDownGroup *group = [[YHTimeCountDownGroup alloc] init];
+        group.interval = interval;
+        group.groupIdentifier = groupIdentifier;
+        
+        dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_main_queue());
+        dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, interval * NSEC_PER_SEC, 0 * NSEC_PER_SEC);
+        dispatch_source_set_event_handler(timer, ^{
+            // 找到所有属于groupIdentifer的models集合
+            NSMutableArray<YHTimeCountDownModel *> *models = [NSMutableArray array];
+            [self.timers enumerateObjectsUsingBlock:^(YHTimeCountDownModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                if ([obj.group.groupIdentifier isEqualToString:groupIdentifier]) {
+                    [models addObject:obj];
+                }
+            }];
+            [models enumerateObjectsUsingBlock:^(YHTimeCountDownModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                obj.currentTimeInterval += obj.group.interval;
+                NSTimeInterval remain = obj.endTimeInterval - (obj.currentTimeInterval + obj.startTimeInterval);
+                if (remain < 0) {
+                    remain = 0;
+                }
+                NSDictionary *info = @{kYHTimeCountDownIdentifierKey : obj.identifier,
+                                       kYHTimeCountDownGroupIdentifierKey : obj.group.groupIdentifier,
+                                       kYHTimeCountDownRemainingTimeKey : @(remain)};
+                
+                [[NSNotificationCenter defaultCenter] postNotificationName:kYHTimeCountDownNotification object:nil userInfo:info];
+                
+//                if (obj.currentTimeInterval >= obj.endTimeInterval) {
+//                    [self removeTimerIntervalWithIdentifer:obj.identifier];
+//                }
+            }];
+        });
+        dispatch_resume(timer);
+        group.timer = timer;
+        model.group = group;
+    } else {
+        model.group = tmpGroup;
     }
     [self.timers addObject:model];
 }
 
-- (void)removeTimerIntervalWithIdentifer:(NSString *)identifer{
+- (void)removeTimerIntervalWithGroupIdentifer:(NSString *)groupIdentifier{
+    __block NSMutableArray<YHTimeCountDownModel *> *models = [NSMutableArray array];
     [self.timers enumerateObjectsUsingBlock:^(YHTimeCountDownModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        if ([obj.identifier isEqualToString:identifer]) {
-            dispatch_source_cancel(obj.timer);
-            obj.timer = nil;
-            [self.timers removeObject:obj];
-            *stop = YES;
+        if ([obj.group.groupIdentifier isEqualToString:groupIdentifier]) {
+            if (obj.group.timer) {
+                dispatch_source_cancel(obj.group.timer);
+                obj.group.timer = nil;
+            }
+            [models addObject:obj];
         }
     }];
+    [self.timers removeObjectsInArray:models];
 }
 
 - (void)removeAllTimer{
     [self.timers enumerateObjectsUsingBlock:^(YHTimeCountDownModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-        dispatch_source_cancel(obj.timer);
-        obj.timer = nil;
+        if (obj.group && obj.group.timer) {
+            dispatch_source_cancel(obj.group.timer);
+            obj.group.timer = nil;
+        }
     }];
     [self.timers removeAllObjects];
 }
 
+
+#pragma mark ------------------ Notification ------------------
+- (void)didEnterBackground:(NSNotification *)noti{
+    /// TODO:即将完成
+}
+
+- (void)willEnterForeground:(NSNotification *)noti{
+    /// TODO:即将完成
+}
 @end
